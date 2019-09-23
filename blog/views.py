0901ad14dcd9b18d325894db4a574999121efd0e
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-09-23 20:36:14 +0200

    urls and some tests

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailsView
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+class BlogDetailsView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
