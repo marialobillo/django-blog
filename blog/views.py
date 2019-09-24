1345cdfcbe17d74bce6b153037694b55da67a76f
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-09-24 12:07:28 +0200

    adding some tests

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailsView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
-class BlogDetailsView(DetailView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
